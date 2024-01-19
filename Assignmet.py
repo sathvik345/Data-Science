54f3fc00e5c1fda16fd39a76c2cbf99c15593f40
sathvik345 <157112077+sathvik345@users.noreply.github.com>
2024-01-19 15:35:00 +0530

    Add files via upload

---
+++ Assignmet.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Tools for Data Science Assignment
+
+# #### This notebook is about the tools for data Science assignment
+
+# ## List of Data Science Languages
+# 1.Python                                                                                                                         
+# 2.R                                                                                                                             
+# 3.SQl                                                                                                                           
+# 4.Julia
+
+# ## List Of Data Science Libraries
+# 1. Pandas                                                                                                                       
+# 2. Numpy
+# 3. SciPy
+# 4. Keras
+
+# In[6]:
+
+
+from IPython.display import Markdown,display
+t = "| Tool | Discription |\n| --- | --- |\n| Numpy | NUmerical Computing Library |\n| Pandas | Data Manipulation and analysis Library |\n| Matplotlib | Data Visualization Library |\n| Keras | High-Level Neural networks API |\n| Seaborn | Data Visualization Library |"
+display(Markdown(t))
+
+
+# ## Below are example of Arithmetic Expressions
+
+# In[7]:
+
+
+#multipy and add numbers
+(4*5)+6
+
+
+# In[9]:
+
+
+#Minutes to Hours
+minutes = 120
+hours = minutes / 60
+print(f"{minutes} minutes is equal to {hours} hours")
+
+
+# ## Objectives 
+# 1. Python                                                                                                                       
+# 2. C++                                                                                                                           
+# 3. Julia
+
+# # Author : K.SATHVIK
+
+# In[ ]:
+
+
+
+
